c39b00009768561c172e05154574dd2ee8f51b92
Arsen Rafailov <arsraf00@gmail.com>
2024-08-07 19:44:34 +0300

    Finish changing existing session function

--- views/training_edit_page.py
+++ views/training_edit_page.py
@@ -21,18 +21,16 @@ class TrainingEditPage():
             session[self.session_title]["sets"] = {}
             session[self.session_title]["time"] = {}
 
-            for exercise in self.session_exercises:
-                exercise_info = []
-                title, *exercise_info = exercise
+            for title in self.session_exercises:
                 session[self.session_title]["order"].append(title)
 
-                if exercise_info[0] == "setsXreps":
+                if "sets" in self.session_exercises[title] and "reps" in self.session_exercises[title]:
                     session[self.session_title]["sets"][title] = {
-                        "set_count": int(exercise_info[1]),
-                        "repeat_count_per_set": int(exercise_info[2])
+                        "set_count": int(self.session_exercises[title]["sets"]),
+                        "repeat_count_per_set": int(self.session_exercises[title]["reps"])
                     }
-                elif exercise_info[0] == "time":
-                    session[self.session_title]["time"][title] = int(exercise_info[1])
+                elif "time" in self.session_exercises[title]:
+                    session[self.session_title]["time"][title] = int(self.session_exercises[title]["time"])
             
             self.page.client_storage.set("trainings", session)
             self.page.go(self.routes["training_page"])
@@ -62,8 +60,8 @@ class TrainingEditPage():
         
         self.session_exercises = self._get_session_exercises(self.session_data)
         session_exercises_items = []
-        for exercise in self.session_exercises:
-            session_exercises_items.append(self._create_exercise_item(exercise))
+        for title in self.session_exercises:
+            session_exercises_items.append(self._create_exercise_item(title, self.session_exercises[title]))
         
         session_list = ft.Container(
             ft.Column(
@@ -79,55 +77,57 @@ class TrainingEditPage():
             scroll=ft.ScrollMode.ADAPTIVE)
     
 
-    def _get_session_exercises(self, session_data: dict) -> list:
+    def _get_session_exercises(self, session_data: dict) -> dict:
         """ Return list with current session exercises """
 
-        session = []
-        
+        session = {}
         for title in session_data["order"]:
             if "sets" in session_data and title in session_data["sets"]:
                 set_count = session_data["sets"][title]["set_count"]
                 reps_count = session_data["sets"][title]["repeat_count_per_set"]
-                session.append((title, "setsXreps", set_count, reps_count))
+                session[title] = {"sets": set_count,
+                                  "reps": reps_count}
             elif "time" in session_data and title in session_data["time"]:
                 seconds = session_data["time"][title]
-                session.append((title, "time", seconds))
+                session[title] = {"time": seconds}
 
         return session
     
 
-    def _create_exercise_item(self, exercise_data: tuple):
+    def _create_exercise_item(self, exercise_title: str, exercise_data) -> ft.Container:
         """ Creates Exercise Control """
-        def delete_exercise(exercise_title: str, exercise: ft.Container):
+        def delete_exercise(exercise_title: str, exercise: ft.Container) -> None:
             """ Trigger For Delete Button """
-            index = None
-            for i in range(len(self.session_exercises)):
-                if self.session_exercises[i][0] == exercise_title:
-                    index = i
-                    break
-            
-            if index is not None:
-                self.session_exercises.pop(i)
-            
+
+            if exercise_title in self.session_exercises:
+                del self.session_exercises[exercise_title]
+
             exercise.disabled = True
             exercise.visible = False
             exercise.update()
+        
 
+        def change_field(e, exercise_title: str, field_type: str) -> None:
+            if "sets" == field_type:
+                self.session_exercises[exercise_title]["sets"] = int(e.data) if e.data != "" else 0
+            elif "reps" == field_type:
+                self.session_exercises[exercise_title]["reps"] = int(e.data) if e.data != "" else 0
+            elif "time" == field_type:
+                self.session_exercises[exercise_title]["time"] = int(e.data) if e.data != "" else 0
 
-        title, exercise_type = exercise_data[0], exercise_data[1]
 
         delete_button = ft.IconButton(
             icon=ft.icons.DELETE, icon_color="#515151", icon_size=25,
             style=ft.ButtonStyle(
                 overlay_color="#cdcdcd"))
+        
+        exercise_title_item = ft.Text(
+            exercise_title.capitalize(),
+            color="#363636", size=20, weight=ft.FontWeight.NORMAL, font_family="Roboto Mono", text_align=ft.TextAlign.CENTER)
+        
+        if "sets" in exercise_data and "reps" in exercise_data:
+            sets_count, reps_count = exercise_data["sets"], exercise_data["reps"]
 
-        if exercise_type == "setsXreps":
-            sets_count, reps_count = exercise_data[2], exercise_data[3]
-
-            exercise_title = ft.Text(
-                title.capitalize(),
-                color="#363636", size=20, weight=ft.FontWeight.NORMAL, font_family="Roboto Mono", text_align=ft.TextAlign.CENTER)
-            
             exercise_sets = ft.CupertinoTextField(
                 width=35,
                 value=sets_count,
@@ -143,8 +143,9 @@ class TrainingEditPage():
                 focused_border_width=0.75,
                 placeholder_text="Set count",
                 placeholder_style=ft.TextStyle(size=25, color="#cdcdcd", font_family="Roboto Mono"),
-                cursor_color="#cdcdcd")
-
+                cursor_color="#cdcdcd",
+                on_change=lambda e: change_field(e, exercise_title, "sets"))
+            
             exercise_reps = ft.CupertinoTextField(
                 width=35,
                 value=reps_count,
@@ -160,23 +161,20 @@ class TrainingEditPage():
                 focused_border_width=0.75,
                 placeholder_text="Rep count",
                 placeholder_style=ft.TextStyle(size=25, color="#cdcdcd", font_family="Roboto Mono"),
-                cursor_color="#cdcdcd")
+                cursor_color="#cdcdcd",
+                on_change=lambda e: change_field(e, exercise_title, "reps"))
 
             exercise_controls = ft.Row(
                 [delete_button,
-                 exercise_sets,
-                 ft.Text("X",
-                         color='#363636', size=20, weight=ft.FontWeight.BOLD, font_family="Roboto Mono", text_align=ft.TextAlign.CENTER),
+                exercise_sets,
+                ft.Text("X",
+                        color='#363636', size=20, weight=ft.FontWeight.BOLD, font_family="Roboto Mono", text_align=ft.TextAlign.CENTER),
                 exercise_reps],
                 alignment=ft.MainAxisAlignment.END,
                 vertical_alignment=ft.CrossAxisAlignment.CENTER)
-        elif exercise_type == "time":
-            seconds = exercise_data[2]
+        elif "time" in exercise_data:
+            seconds = exercise_data["time"]
 
-            exercise_title = ft.Text(
-                title.capitalize(),
-                color="#363636", size=20, weight=ft.FontWeight.NORMAL, font_family="Roboto Mono", text_align=ft.TextAlign.CENTER)
-            
             exercise_time = ft.CupertinoTextField(
                 width=75,
                 value=seconds,
@@ -190,27 +188,29 @@ class TrainingEditPage():
                 border=ft.Border(ft.BorderSide(0.75, "#515151"), ft.BorderSide(0.75, "#515151"), ft.BorderSide(0.75, "#515151"), ft.BorderSide(0.75, "#515151")),
                 focused_border_color="#363636",
                 focused_border_width=0.75,
-                placeholder_text="Set count",
+                placeholder_text="Time (in seconds)",
                 placeholder_style=ft.TextStyle(size=25, color="#cdcdcd", font_family="Roboto Mono"),
-                cursor_color="#cdcdcd")
+                cursor_color="#cdcdcd",
+                on_change=lambda e: change_field(e, exercise_title, "time"))
             
             exercise_controls = ft.Row(
                 [delete_button, exercise_time],
                 alignment=ft.MainAxisAlignment.END,
                 vertical_alignment=ft.CrossAxisAlignment.CENTER)
-        
-        delete_button.on_click = lambda _: delete_exercise(title, exercise)
             
         exercise = ft.Container(
             ft.Row(
-                [exercise_title, exercise_controls],
+                [exercise_title_item, exercise_controls],
                 alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                 vertical_alignment=ft.CrossAxisAlignment.CENTER),
             padding=10,
             border=ft.Border(top=ft.BorderSide(1, "#cdcdcd"), bottom=ft.BorderSide(1, "#cdcdcd")))
         
+        delete_button.on_click = lambda _: delete_exercise(exercise_title, exercise)
+
         return exercise
     
+
     def get_view(self):
         """ Returns view """
 
